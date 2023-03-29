2de2ee242f104c72e52d46849551e3ddad83f321
Roberto Madalin <robertomadalin@yahoo.com>
2023-03-29 17:18:36 +0300

    get user selection from buttons, play a round when user make a selection, update score after a round is played

--- script.js
+++ script.js
@@ -1,53 +1,55 @@
 let choiceArray = ["Rock", "Paper", "Scissors"];
 let playerScore = 0;
 let computerScore = 0;
+let playerSelection;
+let computerSelection;
 
 function getComputerChoice(){
   let selection = Math.floor(Math.random() * 3);
   return choiceArray.at(selection);
 } 
 
+const buttons = document.querySelectorAll("button.selection-button");
+const displayPlayerScore = document.querySelector('#player-score');
+const displayComputerScore = document.querySelector('#computer-score');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    playerSelection = button.textContent;
+    computerSelection = getComputerChoice();
+    playRound(playerSelection,computerSelection);
+    updateScore();
+  })
+})
+
 function playRound(playerSelection, computerSelection){
   if(playerSelection === computerSelection){
-    return ("It`s a tie! " + playerSelection + " ties with " + computerSelection);
+    console.log("It`s a tie! " + playerSelection + " ties with " + computerSelection);
   }else if(playerSelection == "Rock" && computerSelection == "Scissors"){
     playerScore++;
-    return ("You won! " + playerSelection + " beats " + computerSelection);
+    console.log("You won! " + playerSelection + " beats " + computerSelection);
   }else if(playerSelection == "Rock" && computerSelection == "Paper"){
     computerScore++;
-    return("You lose! " + computerSelection + " beats " + playerSelection);
+    console.log("You lose! " + computerSelection + " beats " + playerSelection);
   }else if(playerSelection == "Scissors" && computerSelection == "Rock"){
     computerScore++;
-    return("You lose! " + computerSelection + " beats " + playerSelection);    
+    console.log("You lose! " + computerSelection + " beats " + playerSelection);    
   }else if(playerSelection == "Scissors" && computerSelection == "Paper"){
     playerScore++;
-    return("You won! " + playerSelection + " beats " + computerSelection);
+    console.log("You won! " + playerSelection + " beats " + computerSelection);
   }else if(playerSelection == "Paper" && computerSelection == "Rock"){
     playerScore++;
-    return("You won! " + playerSelection + " beats " + computerSelection);
+    console.log("You won! " + playerSelection + " beats " + computerSelection);
   }else if(playerSelection == "Paper" && computerSelection == "Scissors"){
     computerScore++;
-    return("You lose! " + computerSelection + " beats " + playerSelection);
+    console.log("You lose! " + computerSelection + " beats " + playerSelection);
   }
 }
 
-function getPlayerChoice(){
-  let selection = prompt("Select your weapon between Rock, Paper or Scissors!");
-  return formatPlayerChoice(selection);
+function updateScore(){
+  displayPlayerScore.textContent = playerScore;
+  displayComputerScore.textContent = computerScore;
 }
 
-function formatPlayerChoice(selection){
-  selection = selection.toLowerCase();
-  selection = selection.charAt(0).toUpperCase() + selection.slice(1);
-  return selection;
-}
 
-function game(){
-  for(let i = 0; i <= 4; i++){
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-    console.log(playRound(playerSelection, computerSelection));
-    console.log("Score is: " + playerScore + " - " + computerScore);
-  }
-}
 
